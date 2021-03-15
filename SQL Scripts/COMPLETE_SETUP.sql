a7a0ca4982751ed72a8ee6f028db689b5ea0dc91
Kasper Andersen <31669585+illu12@users.noreply.github.com>
2021-03-15 23:10:41 +0100

    Update COMPLETE_SETUP.sql

--- SQL Scripts/COMPLETE_SETUP.sql
+++ SQL Scripts/COMPLETE_SETUP.sql
@@ -5,7 +5,7 @@
 
 
 -- Drop database if exists, debug only
--- DROP DATABASE IF EXISTS Coderstrust;
+DROP DATABASE IF EXISTS Coderstrust;
 
 -- Create a new database
 CREATE DATABASE Coderstrust;
@@ -231,6 +231,48 @@ INSERT INTO Customer(name) VALUES
 		ON SMI.customer_id = Customer.customer_id
 	;
 /*** End of views **/
+
+
+-- Drop procedure if exists, debug only
+-- DROP PROCEDURE IF EXISTS HighestCourseScore;
+-- Change delimiter for stored procedure
+DELIMITER $$
+-- Create a stored procedure
+CREATE PROCEDURE HighestCourseScore(
+	-- Take integer input
+	IN customer_id_ INT
+)
+-- Start procedure logic
+BEGIN
+	-- Query a table
+	SELECT
+		Course.course_id,
+        	Course.title,
+		SUM(Quiz.score),
+        	Course.url
+	FROM
+		Course
+	-- Get customer data with corresponding customer_id as input
+	JOIN
+		Customer
+	ON
+		Customer.customer_id = customer_id_
+	-- Backtrack and group scores by course
+	JOIN
+		Question
+	ON
+		Question.course_id = Course.course_id
+	JOIN
+		Quiz
+	ON
+		Question.question_id = Quiz.question_id AND Quiz.customer_id = customer_id_
+	GROUP BY
+		Course.course_id ORDER BY SUM(Quiz.score) desc LIMIT 1;
+-- End procedure logic
+END$$
+-- Set default delimiter after procedure
+DELIMITER ;
+
 
 
 # Insert customer answers into quiz
