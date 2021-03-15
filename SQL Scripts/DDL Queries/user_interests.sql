e76779c60a5dcc3f004423510adffd9932d3a70e
Kasper Andersen <31669585+illu12@users.noreply.github.com>
2021-03-15 07:35:31 +0100

    Update user_interests.sql

--- SQL Scripts/DDL Queries/user_interests.sql
+++ SQL Scripts/DDL Queries/user_interests.sql
@@ -11,24 +11,27 @@ CREATE VIEW UserInterests AS
 		Customer.customer_id,
 		Customer.name,
 		-- Generate a percentage value
-        CONCAT(ROUND(GDI.score/(GDI.score+PI.score+SMI.score)*100,0),"%") as graphical_design,
-        CONCAT(ROUND(PI.score/(GDI.score+PI.score+SMI.score)*100,0),"%") as programming,
-        CONCAT(ROUND(SMI.score/(GDI.score+PI.score+SMI.score)*100,0),"%") as social_media
+		CONCAT(ROUND(GDI.score/(GDI.score+PI.score+SMI.score)*100,0),"%") as graphical_design,
+		CONCAT(ROUND(PI.score/(GDI.score+PI.score+SMI.score)*100,0),"%") as programming,
+		CONCAT(ROUND(SMI.score/(GDI.score+PI.score+SMI.score)*100,0),"%") as social_media
 	FROM
 		Customer
+
 	/*** Join all tables, where customer_id is related ***/       
+	
 	-- Rename table as GDI
 	JOIN Graphical_Design_Interest AS GDI
 	ON GDI.customer_id = Customer.customer_id
 
 	-- Rename table as PI
 	JOIN Programming_Interest AS PI
-    ON PI.customer_id = Customer.customer_id
+    	ON PI.customer_id = Customer.customer_id
 
 	-- Rename table as SMI
 	JOIN Social_Media_Interest AS SMI
-    ON SMI.customer_id = Customer.customer_id
+    	ON SMI.customer_id = Customer.customer_id
 ;
 
-SELECT * FROM UserInterests;
+-- Demonstration call
+-- SELECT * FROM UserInterests;
 
